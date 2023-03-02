7a4dd599757c256c18ba109be9dcb13c75c64e64
Bazurogg <bazurogg@gmail.com>
2023-03-02 16:17:23 +0100

    update
    16H

--- chrono.js
+++ chrono.js
@@ -6,10 +6,43 @@ let intervalSeconde
 let intervalMiliseconde
 
 
-function setmiliseconde(){
+function setMiliseconde(){
     if (MS == 999){
         MS = 0
     }
     MS += 1
 document.getElementById('MS').innerHTML = MS + ' ms'    
+}
+
+function setSeconde(){
+    is (S == 59){
+        S = 0
+    }
+    S += 1
+document.getElementById('S').innerHTML = S+' s'    
+}
+
+function setMinute(){
+    if (M == 59){
+        M = 0
+    }
+    M += 1
+document.getElementById('M').innerHTML = M+' min' 
+}
+
+
+function start (){
+    intervalMinute = setInterval (setMinute, 60000)
+    intervalSeconde = setInterval (setSeconde, 1000)
+    intervalMiliseconde = setInterval (setMiliseconde, 1)
+}
+
+function stop (){
+    clearInterval (intervalMinute)
+    clearInterval (intervalSeconde)
+    clearInterval (intervalMiliseconde)   
+}
+function reset (){
+    stop()
+    
 }
